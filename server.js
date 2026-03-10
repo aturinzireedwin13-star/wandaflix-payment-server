08d91db50c4b6afc215a4d4a1ceb1ca1d92a1c57
Aturinzire Edwin <aturinzireedwin@gmail.com>
2026-03-10 19:13:40 +0300

    Add /get-token route

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const CONSUMER_KEY = "CjmavNhVjPUfzdByvopgp0iWy81L75MM";
+const CONSUMER_SECRET = "jTjD/OOj77qJZJrqqFx8HGfzhLM=";
+
+// ROOT TEST ROUTE
+app.get("/", (req, res) => {
+    res.send("Wandaflix Payment Server Running");
+});
+
+// GET PESAPAL TOKEN
+app.get("/get-token", async (req, res) => {
+
+    try {
+
+        const response = await fetch(
+            "https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify({
+                    consumer_key: CONSUMER_KEY,
+                    consumer_secret: CONSUMER_SECRET
+                })
+            }
+        );
+
+        const data = await response.json();
+
+        res.json(data);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            error: "Token request failed"
+        });
+
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
